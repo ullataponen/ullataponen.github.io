8742674f5cc3296554f88eb2b09e407f43f8de90
ullataponen <ulla.taponen@gmail.com>
2020-01-03 18:54:23 +0100

    Improve usability, update portfolio & resume

--- src/index.js
+++ src/index.js
@@ -26,38 +26,38 @@ function topFunction() {
 }
 
 /* Carousel How To by W3Schools:  https://www.w3schools.com/howto/howto_js_slideshow.asp */
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-	showSlides((slideIndex += n));
-}
+// // Next/previous controls
+// function plusSlides(n) {
+// 	showSlides((slideIndex += n));
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-	showSlides((slideIndex = n));
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+// 	showSlides((slideIndex = n));
+// }
 
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length;
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
-}
+// function showSlides(n) {
+// 	var i;
+// 	var slides = document.getElementsByClassName("mySlides");
+// 	var dots = document.getElementsByClassName("dot");
+// 	if (n > slides.length) {
+// 		slideIndex = 1;
+// 	}
+// 	if (n < 1) {
+// 		slideIndex = slides.length;
+// 	}
+// 	for (i = 0; i < slides.length; i++) {
+// 		slides[i].style.display = "none";
+// 	}
+// 	for (i = 0; i < dots.length; i++) {
+// 		dots[i].className = dots[i].className.replace(" active", "");
+// 	}
+// 	slides[slideIndex - 1].style.display = "block";
+// 	dots[slideIndex - 1].className += " active";
+// }
 
 // Contact form security to avoid spambots, from: https://webdesign.tutsplus.com/tutorials/quick-tip-add-a-formspree-form-to-your-static-sites--cms-23870
 var contactform = document.getElementById("contactform");
